44efc97856271ab745f56baeec57aaf4a04e8af5
thaweesap417302 <68282774+thaweesap417302@users.noreply.github.com>
2020-07-14 21:03:58 +0700

    Add files via upload

---
+++ FirstHomeWork.cpp
@@ -0,0 +1,13 @@
+#include<iostream>;
+using namespace std;
+void main();
+{
+    cout<<"    #     "<<endl;
+    cout<<"  #   #   "<<endl;
+    cout<<" #      # "<<endl;
+    cout<<"#        #"<<endl;
+    cout<<"#########"<<endl;
+    cout<<"#        #"<<endl;
+    cunt<<"thaweesap krungsri\n";
+    system("pause");
+}
