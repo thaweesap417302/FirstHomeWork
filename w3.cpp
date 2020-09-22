29db435fd1cd1ceb44faf39f6f324270e8560c38
thaweesap417302 <68282774+thaweesap417302@users.noreply.github.com>
2020-09-22 19:11:47 +0700

    Add files via upload

---
+++ w3.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+int Guess(int);
+int main()
+{
+	int ran,Round;
+	srand(time(NULL));
+
+	ran = rand() % 9 + 1;
+
+	cout << "###Welcome to guessing number game###" << endl;
+	cout << "Secret number has been chosen" << endl;
+
+	Round = Guess(ran);
+
+	cout << "The secret number is " << ran << "" << endl;
+	cout << "You made " << Round << " guesses" << endl;
+
+
+}
+int Guess(int ran) 
+{	int num,Round;
+
+	for (Round = 1;; Round++) {
+		cout << "Guess the number (1 to 10) : ";
+		cin >> num;
+		if (num < ran)
+
+			cout << "The secret number is lower" << endl;
+
+		else if (num > ran)
+
+			cout << "The secret number is higher" << endl;
+
+		else
+		{
+			cout << "Congratulations!" << endl;
+			break;
+		}
+	}
+	return Round;
+}
