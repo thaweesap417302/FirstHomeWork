b436c328ca4a27ffcf173922b1ef1b1d1fbfed80
thaweesap417302 <68282774+thaweesap417302@users.noreply.github.com>
2020-09-30 12:16:09 +0700

    Add files via upload

---
+++ T16.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include <iomanip>
+#include <string>
+using namespace std;
+int main()
+{
+	int num = 0 ;
+	int *quiz = new int[num];
+	int *mid = new int[num];
+	int *final =new int[num];
+	char menu;
+	bool Done = true;
+	do
+	{
+		cout<<setw(24)<<setfill('=')<<endl;
+		cout<<setw(10)<<"MENU"<<endl;
+		cout<<setw(24)<<setfill('=')<<endl;
+		cout<<" 1. Input student records "<<endl;
+		cout<<" 2. View all student records "<<endl;
+		cout<<" 3. Exit "<<endl;
+		cout<<"Seclect menu : ";
+		cin>>menu;
+		if (menu == '1')
+		{
+				cout<<"In put Number of stedent : ";
+				cin>>num;
+
+			for(int x = 0; x<num ; x++)
+			{
+				cout<<"Number"<<x+1<<endl;
+				cout<<"Input Quiz :";
+				cin>>quiz[x];
+				cout<<"Input Midterm :";
+				cin>>mid[x];
+				cout<<"Input Final :";
+				cin>>final[x];
+				cout<<"\n";
+			}
+		}
+		if(menu == '2')
+		{
+			cout<<"------------------------------------------------------------------------"<<endl;
+			cout<<"StdID"<<setw(15)<<setfill(' ')<<"Quiz"<<setw(15)<<setfill(' ')<<"Midterm"<<setw(15)<<setfill(' ')<<"Final"<<endl;
+			for(int i = 0;i<num;i++)
+				cout<<i+1<<setw(17)<<setfill(' ')<<quiz[i]<<setw(12)<<setfill(' ')<<mid[i]<<setw(12)<<setfill(' ')<<final[i]<<setw(17)<<setfill(' ')<<" "<<endl;
+			cout<<"------------------------------------------------------------------------"<<endl;
+		}
+	}while(Done);
+	cout<<"\n...Exit Program...\n";
+
+
+return 0;
+}
